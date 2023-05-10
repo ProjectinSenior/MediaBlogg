bd156ad3cc2ab1297f35a5659a13398f569c1ac0
soumaya <soumayaaouadidridi@gmail.com>
2023-05-10 20:21:36 +0100

    post request done

--- backend/blog/blogControler.js
+++ backend/blog/blogControler.js
@@ -8,4 +8,23 @@ const getpost = (req, res) => {
      else res.json(result)
     })
     };
-module.exports={getpost}
+    const postBlog = (req, res) => {
+        Model.addBlog(
+        
+        req.body.idpost,
+        req.body.tittle,
+        req.body.image,
+        req.body.description,
+        req.body.date,
+        req.body.users_iduser,
+        (err, result) => {
+          if (err) {
+            res.send(err);
+          } else {
+            res.json(result);
+          }
+        })
+        };
+module.exports={getpost,
+                postBlog }
+    
--- backend/blog/blogModel.js
+++ backend/blog/blogModel.js
@@ -1,10 +1,14 @@
-const connection= require ("../database/index.js")
+const connection = require("../database/index.js");
 
 const getAll = (callback) => {
-    const q="select * from post;"
-   connection.query(q, (error, results) => {
-      if (error) throw error;
-        callback(results);
-});
+  const q = "select * from post;";
+  connection.query(q, (error, results) => {
+    if (error) throw error;
+    callback(results);
+  });
 };
-module.exports = {getAll};
+const addBlog = (idpost ,tittle, image, description, date,users_iduser, callback) => {
+  const sql = `INSERT INTO post VALUES (${idpost},"${tittle}","${image}","${description}","${date}",${users_iduser}) `;
+  return connection.query(sql, callback);
+};
+module.exports = { getAll, addBlog };
